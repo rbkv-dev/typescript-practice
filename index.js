63ca330acd65cb47ceb41ee8c1493d3a52580a88
Nikita Rebrikov <rbkv.dev@gmail.com>
2021-03-11 15:20:46 +0200

    Setup build typescript file

---
+++ index.js
@@ -0,0 +1,6 @@
+// --- Types ---
+var any = "Alice" || { name: "Bob" };
+var string = "Alice";
+var number = 21;
+console.log(string);
+// --- Types ---
---
+++ index.ts
@@ -0,0 +1,7 @@
+// --- Types ---
+let any: any = "Alice" || { name: "Bob" };
+let string: string = "Alice";
+let number: number = 21;
+console.log(string);
+
+// --- Types ---
